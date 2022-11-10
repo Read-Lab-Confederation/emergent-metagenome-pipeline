3cd19ed00f33d8e7726a65f0d4d6b7c8961b52a9
Brooke Talbot <bmtalbo@emergent.emory.edu>
2022-11-10 10:16:09 -0500

    added read in bam

--- src/downstream.sh
+++ src/downstream.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+
+# CONDA ENV 
+# conda activate emp 
+
+# GET BAM FILE
+while getopts b:a:f: flag
+do
+    case "${flag}" in
+        b) bamfile=${OPTARG};;
+        a) age=${OPTARG};;
+        f) fullname=${OPTARG};;
+    esac
+done
+echo "bamfile: $bamfile";
+
 
