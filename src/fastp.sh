d504b4cea3e40a362e130059f44fa73ce9a43086
abombin <abombin93@gmail.com>
2022-11-10 15:21:11 +0000

    added fastp script, work dir, and fatp ouput

---
+++ src/fastp.sh
@@ -0,0 +1,6 @@
+fastp --in1 "./data/downsampled_ERR2984773_R1.fastq" --in2 "./data/downsampled_ERR2984773_R2.fastq" \
+--detect_adapter_for_pe \
+--adapter_fasta ./data/NexteraPE-PE.fa \
+--cut_front \
+--out1 "./work/downsampled_ERR2984773_filtered_R1.fastq" \
+--out2 "./work/downsampled_ERR2984773_filtered_R2.fastq"
